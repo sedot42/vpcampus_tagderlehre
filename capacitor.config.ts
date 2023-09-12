2d7514d85c0225ef886b371386bb281ad4a8238c
Friedrich Striewski <friedrich.striewski@fhnw.ch>
2023-09-12 12:50:43 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'campus-v-p-mobile',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
