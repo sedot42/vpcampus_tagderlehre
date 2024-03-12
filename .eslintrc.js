886dd568a4a52cc2686308835360dac8bc5dc994
Stefan Eberlein <stefan.eberlein@fhnw.ch>
2024-03-12 16:07:09 +0100

    enable eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,10 +3,12 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: ["plugin:react/recommended", "eslint:recommended"],
+  extends: ["plugin:react/recommended", "eslint:recommended", 'plugin:@typescript-eslint/recommended'],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 2020,
   },
+  plugins: ['@typescript-eslint'],
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
