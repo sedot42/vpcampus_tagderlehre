f79c3423675b6bccf6b744e1774de580f0e57470
Stefan Eberlein <stefan.eberlein@fhnw.ch>
2024-04-17 17:51:21 +0200

    update eslintrc
    - add plugin for new jsx runtime from react17 to avoid https://github.com/jsx-eslint/eslint-plugin-react/blob/master/docs/rules/react-in-jsx-scope.md
    - add rule no-nested-ternaries

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,14 +3,21 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: ["plugin:react/recommended", "plugin:react-hooks/recommended", "eslint:recommended", 'plugin:@typescript-eslint/recommended'],
-  parser: '@typescript-eslint/parser',
+  extends: [
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/jsx-runtime",
+  ],
+  parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaVersion: 2020,
   },
-  plugins: ['@typescript-eslint'],
+  plugins: ["@typescript-eslint"],
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-nested-ternary": "warn"
   },
 };
