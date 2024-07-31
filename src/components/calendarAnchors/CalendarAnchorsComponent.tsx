385c01f08c96f6da879982f6c71b8aaed770698f
Dario Jaeggi <dario.jaeggi@fhnw.ch>
2024-07-31 11:12:40 +0200

    Added fullweek/workweek option

--- src/components/calendarAnchors/CalendarAnchorsComponent.tsx
+++ src/components/calendarAnchors/CalendarAnchorsComponent.tsx
@@ -1,10 +1,11 @@
-import { IonPage } from "@ionic/react";
+import { IonContent, IonPage, IonToggle } from "@ionic/react";
 import { StatusHeader } from "../globalUI/StatusHeader";
 
 // Fullcalendar imports
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid"; // a plugin!
 import interactionPlugin, { DateClickArg } from "@fullcalendar/interaction"; // needed for dayClick
+import { useState } from "react";
 
 export const CalendarAnchorComponent = () => {
   const handleDateClick = (date: DateClickArg) => {
@@ -12,19 +13,25 @@ export const CalendarAnchorComponent = () => {
     alert(date.dateStr);
   };
 
+  const [displayWeekends, setDisplayWeekends] = useState(true);
+
   return (
     <IonPage>
       <StatusHeader titleText="Kalender" />
-      {/* <IonContent className="ion-padding"> */}
 
-      <FullCalendar
-        height="100%" // Set height of Calender to fill whole container -> Must be later set to something different/auto if other children elements are added
-        plugins={[dayGridPlugin, interactionPlugin]}
-        dateClick={handleDateClick}
-        initialView="dayGridMonth"
-        weekends={false}
-      />
-      {/* </IonContent> */}
+      <IonContent className="ion-padding">
+        <FullCalendar
+          height="auto" //"100%"  Set height of Calender to fill whole container -> Must be later set to something different/auto if other children elements are added
+          plugins={[dayGridPlugin, interactionPlugin]}
+          dateClick={handleDateClick}
+          initialView="dayGridMonth"
+          weekends={displayWeekends}
+        />
+        {/* Button to change fullweek/workweek -> Button and state should be moved to options */}
+        <IonToggle onClick={() => setDisplayWeekends(displayWeekends ? false : true)}>
+          Ganze Woche / Arbeitswoche
+        </IonToggle>
+      </IonContent>
     </IonPage>
   );
 };
