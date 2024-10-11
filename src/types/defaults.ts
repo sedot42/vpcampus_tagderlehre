80fa84a1a89f5bcdf3364fc638a4a230fb383363
Stefan Eberlein <stefan.eberlein@fhnw.ch>
2024-10-11 16:36:52 +0200

    minor improvements

--- src/types/defaults.ts
+++ src/types/defaults.ts
@@ -1,7 +1,6 @@
 import { DraftAnchor, Anchor } from "./types";
 
 export const draftAnchor: DraftAnchor<Anchor> = {
-
   anchor_name: "Bespiel-Objekt",
   owner_id: "550e8400-e29b-41d4-a716-446655440000",
 };
