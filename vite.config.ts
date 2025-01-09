38147b707b6ddb770230f007f37492fd0c0cd97c
Stefan Eberlein <stefan.eberlein@fhnw.ch>
2025-01-09 16:26:04 +0100

    reformat vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,9 @@
-import legacy from '@vitejs/plugin-legacy'
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import legacy from "@vitejs/plugin-legacy";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    legacy()
-  ],
+  plugins: [react(), legacy()],
   base: "./",
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/setupTests.ts',
-  }
-})
+});
