d21a44752f990837550e3441ca1ecd6fb55ed8c2
Stefan Eberlein <stefan.eberlein@fhnw.ch>
2025-06-17 14:33:40 +0200

    add PWA support

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,19 @@
 import legacy from "@vitejs/plugin-legacy";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), legacy()],
+  plugins: [
+    react(),
+    legacy(),
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: {
+        enabled: true,
+      },
+    }),
+  ],
   base: "./",
 });
